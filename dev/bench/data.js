8f3eba78da2bf2acc659b36cc9e2e3921588b4b6
github-action-benchmark <github@users.noreply.github.com>
2024-01-22 06:59:37 +0000

    add Benchmarks (benchmarkdotnet) benchmark result for 1e67149d2163b43e1a983e67fdb2618700d4106e

---
+++ dev/bench/data.js
@@ -0,0 +1,44 @@
+window.BENCHMARK_DATA = {
+  "lastUpdate": 1705906777448,
+  "repoUrl": "https://github.com/destructurama/by-ignoring",
+  "entries": {
+    "Benchmarks": [
+      {
+        "commit": {
+          "author": {
+            "email": "sungam3r@yandex.ru",
+            "name": "Ivan Maximov",
+            "username": "sungam3r"
+          },
+          "committer": {
+            "email": "noreply@github.com",
+            "name": "GitHub",
+            "username": "web-flow"
+          },
+          "distinct": true,
+          "id": "1e67149d2163b43e1a983e67fdb2618700d4106e",
+          "message": "Get already configured policy (#43)",
+          "timestamp": "2024-01-22T09:58:15+03:00",
+          "tree_id": "04f0d3a21aa19277c3598991ce0bc6b6cc9f522e",
+          "url": "https://github.com/destructurama/by-ignoring/commit/1e67149d2163b43e1a983e67fdb2618700d4106e"
+        },
+        "date": 1705906776337,
+        "tool": "benchmarkdotnet",
+        "benches": [
+          {
+            "name": "Benchmarks.ByIgnoringBenchmarks.Destructure",
+            "value": 131.77229829934927,
+            "unit": "ns",
+            "range": "± 0.2636294843881182"
+          },
+          {
+            "name": "Benchmarks.ByIgnoringBenchmarks.DestructureAssignable",
+            "value": 132.46599239110947,
+            "unit": "ns",
+            "range": "± 0.33372790636186317"
+          }
+        ]
+      }
+    ]
+  }
+}
